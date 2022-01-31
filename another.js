fd8e8a5f828b6d83c32da1250ecef7a4a5390f3f
WeCode 4 <wecode4@Troelss-Mac-mini.local>
2022-01-31 13:22:47 +0100

    Make it so

---
+++ another.js
@@ -0,0 +1,24 @@
+(function() {
+
+
+  var Person = (function () {
+    function Person() {
+
+    }
+    Person.prototype = {
+
+    };
+    return Person;
+  }());
+
+  var Animal = (function () {
+    function Animal() {
+
+    }
+    Animal.prototype = {
+
+    };
+
+    return Animal;
+  }());
+}());
